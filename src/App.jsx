22b66e5a1f346fcd4b22dab5562e616af8c939e1
Stephen Muñez <sjtmunez@gmail.com>
2025-08-05 14:12:39 +0800

    feat: tailwind config and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl font-bold text-orange-500">Welcome to Zentry</h1>
+      <h1 className="text-5xl font-bold text-violet-300">Welcome to Zentry</h1>
     </main>
   );
 };
