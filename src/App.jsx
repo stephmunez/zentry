e0e1dc230c58715bb63e574161ebbe5a86ff99ee
Stephen Muñez <sjtmunez@gmail.com>
2025-08-05 16:07:22 +0800

    feat: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Hero from './components/Hero';
+
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl font-bold text-violet-300">Welcome to Zentry</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
   );
 };
