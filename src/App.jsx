3fa76f250b9c86a7f5bf76976782b803278845e1
Stephen Muñez <sjtmunez@gmail.com>
2025-08-06 12:06:29 +0800

    feat: about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from './components/About';
 import Hero from './components/Hero';
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <About />
       <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
   );
